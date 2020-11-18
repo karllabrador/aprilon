137ebabc482fa242f5c7d8fee3ae63a3832d7a63
Karl Labrador <karl.labrad@gmail.com>
2020-11-18 04:58:55 +0100

    Added clientside JS for cards and download button animation

--- public/js/aprilon.js
+++ public/js/aprilon.js
@@ -9,31 +9,56 @@
 const cards = {};
 
 cards.config = {
-    url: '/cards.json'
+    url: '/cards.json',
+    selector: '.cards'
 }
 
 cards.fetchData = () => {
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open('GET', this.config.url, false);
-    xmlHttp.send(null);
+    $.getJSON(cards.config.url, data => {
+       let items = [];
 
-    if (xmlHttp.responseText) {
-        if (JSON) {
-            return JSON.stringify(xmlHttp.responseText);
-        }
-    }
-}
+       $.each(data, function(key, val) {
+           let friendid = val.friendid;
+           let desc = ((val.desc) ? val.desc : '');
+           let steam_name = val.steam_name;
+           let steam_avatar_url = val.steam_avatar_url;
+           let steam_state_class = cards.getStateClass(val.steam_state);
 
-cards.buildCard = data => {
+           let html =
+               '<div class="column is-one-third">' +
+               '    <div class="card">'+
+               '        <div class="avatar is-pulled-left' + steam_state_class + '">' +
+               '            <img src="' + steam_avatar_url + '" />' +
+               '        </div>' +
+               '        <div class="details is-pulled-left">' +
+               '            <p class="name is-size-4"><a href="https://steamcommunity.com/profiles/' + friendid + '" class="has-text-light">' + steam_name + '</a></p>' +
+               '            <p class="heading">' + desc + '</p>' +
+               '        </div>' +
+               '        <div style="display:inline-block" class="is-pulled-right"><p></p></div>' +
+               '        <div class="is-clearfix"></div>' +
+               '    </div>' +
+               '</div>'
 
-}
+           items.push(html);
+       });
 
-cards.run = () => {
-    let data = this.fetchData();
-    data.forEach((card) => {
-        this.buildCard(card);
+       items.forEach((item) => {
+          $(cards.config.selector).append(item);
+       });
     });
 }
+
+/**
+ * Evaluates the personastate code, where 0 is offline, 1-6 is online and 7 is in-game
+ * @param c
+ * @returns {string}
+ */
+cards.getStateClass = c => {
+    if (c === 7) return ' is-ingame';
+    if (c >= 1) return ' is-online';
+    else return ' ';
+}
+
 /**
  * Counting Animation
  */
@@ -46,7 +71,6 @@ countUp.config = {
 
 countUp.animate = e => {
     let size = e.html().split(".")[1] ? e.html().split(".")[1].length : 0;
-    console.log('Size: ' + size);
     e.prop('Counter', 0).animate({
         Counter: e.text()
     }, {
@@ -65,5 +89,19 @@ countUp.run = () => {
 }
 
 $(function() {
+    // Look for countups and run animation
     countUp.run();
+
+    // Fetch cards data
+    cards.fetchData();
+
+    // Loading animation for download buttons
+    $('.download-button').click(function() {
+        const el = $(this);
+        el.addClass("is-loading");
+
+        setInterval(function() {
+            el.removeClass("is-loading");
+        }, 2000)
+    })
 });

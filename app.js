fa7b5ac8746c4111acf869a072b069ee0701ccf4
Karl Labrador <karl.labrad@gmail.com>
2020-11-18 04:58:05 +0100

    Cards are updated on server start

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var cron = require('node-cron');
+const cards = require(path.join(__dirname, 'jobs', 'cards'));
 
 var indexRouter = require('./routes/index');
+var downloadRouter = require('./routes/download');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// generate cards once and then run every 5 minutes
+cards.generate();
+cron.schedule('*/5 * * * *', () => {
+  cards.generate();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/download', downloadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

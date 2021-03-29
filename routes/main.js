3cca2967e5ebc055a49879e4f4bd69c4bc9a1f6d
Karl Labrador <karl.labrad@gmail.com>
2021-03-29 17:49:47 +0200

    Readable if-else in dl/disc

--- routes/main.js
+++ routes/main.js
@@ -14,17 +14,21 @@ router.get('/', (req, res, next) => {
 const worlds = config['minecraft_worlds'];
 
 router.get('/download/world-:worldId', (req, res) => {
-   const worldId = req.params.worldId;
-   const world_url = worlds[worldId];
+    const worldId = req.params.worldId;
+    const world_url = worlds[worldId];
 
-    (world_url ? res.redirect(world_url) : res.status(400).send('Minecraft World is missing in the config'));
+    world_url
+        ? res.redirect(world_url)
+        : res.status(400).send('Minecraft World is missing in the config');
 });
 
 /* GET discord */
 const discord_url = config['discord'];
 
 router.get('/discord', (req, res) => {
-    (discord_url ? res.redirect(discord_url) : res.status(400).send('Discord URL is missing in the config.'));
+    discord_url
+        ? res.redirect(discord_url)
+        : res.status(400).send('Discord URL is missing in the config.');
 });
 
 module.exports = router;

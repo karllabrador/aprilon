6f99efddcb19f4ac10a3f20a6f9d9511f263cdb2
Karl Labrador <karl.labrad@gmail.com>
2021-03-30 03:29:31 +0200

    Update .travis.yml, changed prettier config & performed write

--- classes/cards.js
+++ classes/cards.js
@@ -1,144 +1,144 @@
-const path = require("path");
-const fs = require("fs");
-const sprintf = require("sprintf-js").sprintf;
-const config = require(path.join(__dirname, "..", "config", "config"));
-const http = require("http");
-const SteamID = require("steamid");
-const Card = require("./card");
+const path = require('path');
+const fs = require('fs');
+const sprintf = require('sprintf-js').sprintf;
+const config = require(path.join(__dirname, '..', 'config', 'config'));
+const http = require('http');
+const SteamID = require('steamid');
+const Card = require('./card');
 
 const STEAM_API_QUERY_URL =
-  "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s&format=json";
-const STEAM_API_KEY = config["cards"]["steam_api_key"];
+    'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s&format=json';
+const STEAM_API_KEY = config['cards']['steam_api_key'];
 
 class Cards {
-  /**
-   * Constructor
-   * @param referenceFile relative file path to the reference JSON file
-   * @param outputFile relative output file path for produced JSON
-   */
-  constructor(referenceFile, outputFile) {
-    this.cards = [];
-    this.referenceFile = referenceFile;
-    this.outputFile = outputFile;
-  }
-
-  /**
-   * Builds the Steam API query URL
-   * @returns {string|void}
-   */
-  getQueryURL() {
-    if (this.cards.length === 1)
-      return console.error("Cards have not been initialized yet");
-
-    if (!STEAM_API_KEY) return console.error("Steam API key is undefined");
-
-    const joinedIds = this.cards.map((e) => e.friendid).join();
-
-    return sprintf(STEAM_API_QUERY_URL, STEAM_API_KEY, joinedIds);
-  }
-
-  /**
-   * Fills the cards array with data from the reference file
-   */
-  init() {
-    if (this.cards.length !== 0) return;
-
-    const contributors = this.loadCont();
-
-    contributors.forEach((cont) => {
-      const name = cont.name;
-      const steamid = cont.steamid;
-
-      const card = new Card(name, steamid);
-
-      card.desc = cont.desc ? cont.desc : "";
-      card.discord = cont.discord ? cont.discord : "";
-      card.github = cont.github ? cont.github : "";
-
-      this.cards.push(card);
-    });
-  }
-
-  /**
-   * Loads the reference file and parses the JSON content
-   * @returns {any}
-   */
-  loadCont() {
-    const filePath =
-      this.referenceFile ||
-      path.join(__dirname, "..", "config", "contributors.json");
-    let contents;
-
-    try {
-      contents = () => fs.readFileSync(filePath);
-    } catch (err) {
-      console.error(err);
+    /**
+     * Constructor
+     * @param referenceFile relative file path to the reference JSON file
+     * @param outputFile relative output file path for produced JSON
+     */
+    constructor(referenceFile, outputFile) {
+        this.cards = [];
+        this.referenceFile = referenceFile;
+        this.outputFile = outputFile;
     }
 
-    return JSON.parse(contents().toString());
-  }
+    /**
+     * Builds the Steam API query URL
+     * @returns {string|void}
+     */
+    getQueryURL() {
+        if (this.cards.length === 1)
+            return console.error('Cards have not been initialized yet');
 
-  /**
-   * Writes the cards array as JSON data to the output file
-   */
-  write() {
-    const outputPath =
-      this.outputFile ||
-      path.join(__dirname, "..", "public", "data", "cards.json");
+        if (!STEAM_API_KEY) return console.error('Steam API key is undefined');
 
-    fs.writeFile(outputPath, JSON.stringify(this.cards), (err) => {
-      if (err) return console.error(err);
+        const joinedIds = this.cards.map(e => e.friendid).join();
 
-      console.log("Wrote cards to file");
-    });
-  }
+        return sprintf(STEAM_API_QUERY_URL, STEAM_API_KEY, joinedIds);
+    }
 
-  /**
-   * Runs the Steam API request and fetches profile data, then stores it in Card objects
-   */
-  run() {
-    this.init();
-    const queryURL = this.getQueryURL();
+    /**
+     * Fills the cards array with data from the reference file
+     */
+    init() {
+        if (this.cards.length !== 0) return;
 
-    http.get(queryURL, (res) => {
-      const chunks = [];
+        const contributors = this.loadCont();
 
-      res.on("data", (data) => {
-        chunks.push(data);
-      });
+        contributors.forEach(cont => {
+            const name = cont.name;
+            const steamid = cont.steamid;
 
-      res.on("end", () => {
-        const data = Buffer.concat(chunks);
-        let playerdata; // playerdata
+            const card = new Card(name, steamid);
+
+            card.desc = cont.desc ? cont.desc : '';
+            card.discord = cont.discord ? cont.discord : '';
+            card.github = cont.github ? cont.github : '';
+
+            this.cards.push(card);
+        });
+    }
+
+    /**
+     * Loads the reference file and parses the JSON content
+     * @returns {any}
+     */
+    loadCont() {
+        const filePath =
+            this.referenceFile ||
+            path.join(__dirname, '..', 'config', 'contributors.json');
+        let contents;
 
         try {
-          playerdata = JSON.parse(data.toString());
+            contents = () => fs.readFileSync(filePath);
         } catch (err) {
-          console.error("Error fetching playerdata", err);
+            console.error(err);
         }
 
-        playerdata = playerdata.response.players;
+        return JSON.parse(contents().toString());
+    }
 
-        playerdata.forEach((ply) => {
-          const friendid = ply.steamid;
-          const personaname = ply.personaname;
-          const avatarmedium = ply.avatarmedium;
+    /**
+     * Writes the cards array as JSON data to the output file
+     */
+    write() {
+        const outputPath =
+            this.outputFile ||
+            path.join(__dirname, '..', 'public', 'data', 'cards.json');
 
-          const personastate = ply.gameid ? 7 : ply.personastate;
+        fs.writeFile(outputPath, JSON.stringify(this.cards), err => {
+            if (err) return console.error(err);
 
-          // Set data to card
-          const card = this.cards.find((e) => e.friendid === friendid);
-          if (card) {
-            card.steam_name = personaname;
-            card.steam_state = personastate;
-            card.steam_avatar_url = avatarmedium;
-          }
+            console.log('Wrote cards to file');
         });
+    }
 
-        this.write();
-      });
-    });
-  }
+    /**
+     * Runs the Steam API request and fetches profile data, then stores it in Card objects
+     */
+    run() {
+        this.init();
+        const queryURL = this.getQueryURL();
+
+        http.get(queryURL, res => {
+            const chunks = [];
+
+            res.on('data', data => {
+                chunks.push(data);
+            });
+
+            res.on('end', () => {
+                const data = Buffer.concat(chunks);
+                let playerdata; // playerdata
+
+                try {
+                    playerdata = JSON.parse(data.toString());
+                } catch (err) {
+                    console.error('Error fetching playerdata', err);
+                }
+
+                playerdata = playerdata.response.players;
+
+                playerdata.forEach(ply => {
+                    const friendid = ply.steamid;
+                    const personaname = ply.personaname;
+                    const avatarmedium = ply.avatarmedium;
+
+                    const personastate = ply.gameid ? 7 : ply.personastate;
+
+                    // Set data to card
+                    const card = this.cards.find(e => e.friendid === friendid);
+                    if (card) {
+                        card.steam_name = personaname;
+                        card.steam_state = personastate;
+                        card.steam_avatar_url = avatarmedium;
+                    }
+                });
+
+                this.write();
+            });
+        });
+    }
 }
 
 module.exports = Cards;
--- routes/main.js
+++ routes/main.js
@@ -1,34 +1,34 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const path = require("path");
-const config = require(path.join(__dirname, "..", "config", "config"));
+const path = require('path');
+const config = require(path.join(__dirname, '..', 'config', 'config'));
 
 /* GET home page. */
-router.get("/", (req, res, next) => {
-  res.render("index", {
-    title: "Aprilon — gaming community (now defunct)",
-  });
+router.get('/', (req, res, next) => {
+    res.render('index', {
+        title: 'Aprilon — gaming community (now defunct)',
+    });
 });
 
 /* GET download */
-const worlds = config["minecraft_worlds"];
+const worlds = config['minecraft_worlds'];
 
-router.get("/download/world-:worldId", (req, res) => {
-  const worldId = req.params.worldId;
-  const world_url = worlds[worldId];
+router.get('/download/world-:worldId', (req, res) => {
+    const worldId = req.params.worldId;
+    const world_url = worlds[worldId];
 
-  world_url
-    ? res.redirect(world_url)
-    : res.status(400).send("Minecraft World is missing in the config");
+    world_url
+        ? res.redirect(world_url)
+        : res.status(400).send('Minecraft World is missing in the config');
 });
 
 /* GET discord */
-const discord_url = config["discord"];
+const discord_url = config['discord'];
 
-router.get("/discord", (req, res) => {
-  discord_url
-    ? res.redirect(discord_url)
-    : res.status(400).send("Discord URL is missing in the config.");
+router.get('/discord', (req, res) => {
+    discord_url
+        ? res.redirect(discord_url)
+        : res.status(400).send('Discord URL is missing in the config.');
 });
 
 module.exports = router;

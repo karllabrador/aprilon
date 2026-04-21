a5aeca14df1e72707a585f005f2ae37303cb3095
Karl Labrador <karl.labrad@gmail.com>
2026-04-21 22:13:04 +0200

    Add pulse animation to Discord live number

--- components/common/DiscordSplitButton.tsx
+++ components/common/DiscordSplitButton.tsx
@@ -19,7 +19,10 @@ export default function DiscordSplitButton({
         Discord
       </span>
       <span className="inline-flex items-center gap-2 px-3 py-2.5 normal-case tracking-normal">
-        <span className="inline-block w-2 h-2 rounded-full bg-[#3ba55c]" />
+        <span className="relative flex h-2 w-2">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-[#3ba55c] opacity-75" />
+          <span className="relative inline-flex rounded-full h-2 w-2 bg-[#3ba55c]" />
+        </span>
         <span className="text-white/65">
           {memberCount.toLocaleString()}
         </span>

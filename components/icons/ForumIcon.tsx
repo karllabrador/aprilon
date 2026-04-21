d7dcfcc558e41fe24364ad95248493476e65cc2d
Karl Labrador <karl.labrad@gmail.com>
2026-04-21 22:07:46 +0200

    Add SVG icons

---
+++ components/icons/ForumIcon.tsx
@@ -0,0 +1,21 @@
+export default function ForumIcon() {
+  return (
+    <svg
+      viewBox="0 0 64 64"
+      width={64}
+      height={64}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="2" y="2" width="42" height="32" rx="5" fill="#EDD68A" />
+      <rect x="2" y="28" width="8" height="8" rx="1" fill="#EDD68A" />
+      <circle cx="14" cy="18" r="3" fill="#C8A84B" />
+      <circle cx="23" cy="18" r="3" fill="#C8A84B" />
+      <circle cx="32" cy="18" r="3" fill="#C8A84B" />
+      <rect x="20" y="24" width="42" height="32" rx="5" fill="#EDD68A" />
+      <rect x="54" y="48" width="8" height="8" rx="1" fill="#EDD68A" />
+      <circle cx="32" cy="40" r="3" fill="#C8A84B" />
+      <circle cx="41" cy="40" r="3" fill="#C8A84B" />
+      <circle cx="50" cy="40" r="3" fill="#C8A84B" />
+    </svg>
+  );
+}
